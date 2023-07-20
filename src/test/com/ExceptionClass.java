06cfe74f5115ac741f5d03e5943d9202d509e93a
ThooyavanMS <thooyavan081@gmail.com>
2023-07-20 11:57:15 +0530

    try exception

---
+++ src/test/com/ExceptionClass.java
@@ -0,0 +1,16 @@
+package test.com;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExceptionClass {
+	public static void main(String[] args) {
+		Map<Integer, String> m=new HashMap<>();
+		m.put(1, "qwert");
+		m.put(2, "asdfgh");
+		m.put(3, "zxcvb");
+		m.put(4, "ioplk");
+		
+		}
+	}
+	
